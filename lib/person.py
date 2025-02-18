5cd0c9019d6d3e867868a2fbc762c10624e77c7d
Patty Karigi Kariuki <pattykarigi96@gmail.com>
2025-02-18 16:20:06 +0300

    Initial commit

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,14 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+        return "Hello World!"
+    
+    def walk(self):
+        print("The person is walking.")
+        return "The person is walking."
+    
+patty = Person()
+patty.talk()
+patty.walk()
